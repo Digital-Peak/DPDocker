e7399b24ac39682c07e3a11444ab7a62b816c0ea
Allon Moritz <allon.moritz@digital-peak.com>
2020-08-11 14:47:11 +0200

    Support joomla installation without internet connection

--- webserver/scripts/setup-joomla-site.php
+++ webserver/scripts/setup-joomla-site.php
@@ -5,30 +5,37 @@
  * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 
-$wwwRoot = '/var/www/html/' . $argv[1];
-$db      = array_key_exists(3, $argv) ? $argv[3] : 'mysql';
-$force   = array_key_exists(4, $argv) ? (bool)$argv[4] : false;
-$binary  = '/home/docker/vendor/bin/joomla';
+$hasInternet = true;
+$wwwRoot     = '/var/www/html/' . $argv[1];
+$db          = array_key_exists(3, $argv) ? $argv[3] : 'mysql';
+$force       = array_key_exists(4, $argv) ? (bool)$argv[4] : false;
+$binary      = '/home/docker/vendor/bin/joomla';
 
 if (is_dir($wwwRoot) && !$force) {
 	return;
 }
 
 if (!is_dir($wwwRoot) || $force) {
-	if (!is_dir('/var/www/html/cache')) {
+	if (!is_dir('/var/www/html/cache') && $hasInternet) {
 		shell_exec('git clone https://github.com/joomla/joomla-cms.git /var/www/html/cache 2>&1 > /dev/null');
-	} else {
+	} else if ($hasInternet) {
 		shell_exec('git --work-tree=/var/www/html/cache --git-dir=/var/www/html/cache/.git fetch origin 2>&1 > /dev/null');
+	} else if (is_dir('/var/www/html/Projects/DPDocker/webserver/www/cache')) {
+		shell_exec('cp -r /var/www/html/Projects/DPDocker/webserver/www/cache /var/www/html/cache');
+	} else {
+		echo 'Can not setup Joomla!!!!';
+
+		return;
 	}
 
 	shell_exec('rm -rf ' . $wwwRoot);
 	shell_exec('cp -r /var/www/html/cache ' . $wwwRoot);
 
-	if (substr($argv[1], -1) == 4) {
+	if (substr($argv[1], -1) == 4 && $hasInternet) {
 		// Checkout latest stable release
 		shell_exec('git --work-tree=' . $wwwRoot . ' --git-dir=' . $wwwRoot . '/.git checkout tags/4.0.0-beta3 2>&1 > /dev/null');
 		echo 'Using version 4.0.0-beta3 on ' . $wwwRoot . PHP_EOL;
-	} else {
+	} else if ($hasInternet) {
 		$versions = json_decode(file_get_contents('https://downloads.joomla.org/api/v1/latest/cms'));
 		foreach ($versions->branches as $branch) {
 			if ($branch->branch !== 'Joomla! 3') {
@@ -39,6 +46,8 @@ if (!is_dir($wwwRoot) || $force) {
 			rename($wwwRoot . '/installation', $wwwRoot . '/_installation');
 			echo 'Using version ' . $branch->version . ' on ' . $wwwRoot . PHP_EOL;
 		}
+	} else {
+		rename($wwwRoot . '/installation', $wwwRoot . '/_installation');
 	}
 }
 echo shell_exec('/var/www/html/Projects/DPDocker/webserver/scripts/install-joomla.sh ' . $wwwRoot . ' ' . $db . ' sites_' . $argv[1] . ' "Joomla ' . $argv[1] . '" mailcatcher');
@@ -69,9 +78,11 @@ foreach ($folders as $project) {
 		continue;
 	}
 
-	echo 'Building extension ' . $project . PHP_EOL;
-	shell_exec('/var/www/html/Projects/DPDocker/composer/scripts/exec-install.sh ' . $project);
-	shell_exec('/var/www/html/Projects/DPDocker/npm/scripts/exec-npm-install.sh ' . $project . ' 2>/dev/null');
+	if ($hasInternet) {
+		echo 'Building extension ' . $project . PHP_EOL;
+		shell_exec('/var/www/html/Projects/DPDocker/composer/scripts/exec-install.sh ' . $project);
+		shell_exec('/var/www/html/Projects/DPDocker/npm/scripts/exec-npm-install.sh ' . $project . ' 2>/dev/null');
+	}
 
 	createLinks('/var/www/html/Projects/' . $project . '/', $wwwRoot);
 }

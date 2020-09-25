c57a7340d644590eb240259f3d6c8af0c27fefa6
astridx <astrid.guenther@dimna.de>
2020-09-25 21:13:39 +0200

    hint if you forgett the extension name

--- build/run.sh
+++ build/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+  echo "Did you forget to add the name of the extension as a parameter to the command? Only the version number is optional."
+  exit 1
+fi
+
 EXTENSION=$1 VERSION=$2 docker-compose -f $(dirname $0)/docker-compose.yml run --rm build

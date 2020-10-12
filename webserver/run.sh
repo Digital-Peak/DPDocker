dd47b736cd5966bd48d5d0d4bab3ee1cde6d3ef0
astridx <astrid.guenther@dimna.de>
2020-10-12 11:00:02 +0200

    add parameter for mysql version in webserver/run.sh

--- webserver/run.sh
+++ webserver/run.sh
@@ -5,5 +5,10 @@ if [ ! -d $(dirname $0)/www ]; then
   mkdir $(dirname $0)/www
 fi
 
+# TODO
+# Delete folder mysql_data if mysql verion changed from 5.x to 8
+# Fallback for DBVERSION that is no mysql tag in image https://hub.docker.com/_/mysql
+# Add changes to readme
+
 # Start the dev server
-DB=$1 REBUILD=$2 docker-compose -f $(dirname $0)/docker-compose.yml up
+DB=$1 REBUILD=$2 DBVERSION=$3 docker-compose -f $(dirname $0)/docker-compose.yml up

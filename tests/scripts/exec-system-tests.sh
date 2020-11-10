88e8e3341d8406c50775f33a74d361b2568b60bf
Allon Moritz <allon.moritz@digital-peak.com>
2020-11-10 11:06:16 +0100

    Enable extension as cli argument

--- tests/scripts/exec-system-tests.sh
+++ tests/scripts/exec-system-tests.sh
@@ -34,4 +34,4 @@ if [ -d acceptance/install ]; then
 fi
 
 # Run the tests
-vendor/bin/codecept run --env desktop acceptance
+vendor/bin/codecept run --env desktop --ext "Codeception\ProgressReporter\ProgressReporter" acceptance

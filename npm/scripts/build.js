2962b605e92a6b29662e6e421353a0b4c5fefcf9
astridx <info@astrid-guenther.de>
2020-12-09 19:48:46 +0100

    corrections

--- npm/scripts/build.js
+++ npm/scripts/build.js
@@ -84,10 +84,8 @@ function buildAssets(root, assets, includeVendor)
 			if (index == 0) {
 				if (fs.statSync(file).isFile()) {
 					fs.copyFileSync(file, root + '/' + asset.dest);
-					console.log('File ' + file + ' copied to ' + root + '/' + asset.dest);
 				} else {
 					copyFolderRecursiveSync(file, root + '/' + asset.dest);
-					console.log('Folder  ' + file + ' copied to ' + root + '/' + asset.dest);
 				}
 				return;
 			}
@@ -150,38 +148,38 @@ function buildAssets(root, assets, includeVendor)
 	});
 }
 
-function copyFileSync( source, target ) {
+function copyFileSync(source, target) {
 
     var targetFile = target;
 
     // If target is a directory, a new file with the same name will be created
-    if ( fs.existsSync( target ) ) {
-        if ( fs.lstatSync( target ).isDirectory() ) {
-            targetFile = path.join( target, path.basename( source ) );
+    if (fs.existsSync(target)) {
+        if (fs.lstatSync(target).isDirectory()) {
+            targetFile = path.join(target, path.basename(source));
         }
     }
 
     fs.writeFileSync(targetFile, fs.readFileSync(source));
 }
 
-function copyFolderRecursiveSync( source, target ) {
+function copyFolderRecursiveSync(source, target) {
     var files = [];
 
     // Check if folder needs to be created or integrated
     var targetFolder = path.join( target, path.basename( source ) );
-    if ( !fs.existsSync( targetFolder ) ) {
-        fs.mkdirSync( targetFolder );
+    if (!fs.existsSync(targetFolder)) {
+        fs.mkdirSync(targetFolder);
     }
 
     // Copy
-    if ( fs.lstatSync( source ).isDirectory() ) {
+    if (fs.lstatSync(source).isDirectory()) {
         files = fs.readdirSync( source );
         files.forEach( function ( file ) {
-            var curSource = path.join( source, file );
-            if ( fs.lstatSync( curSource ).isDirectory() ) {
-                copyFolderRecursiveSync( curSource, targetFolder );
+            var curSource = path.join(source, file);
+            if (fs.lstatSync(curSource).isDirectory()) {
+                copyFolderRecursiveSync(curSource, targetFolder);
             } else {
-                copyFileSync( curSource, targetFolder );
+                copyFileSync(curSource, targetFolder);
             }
         } );
     }

98210f79b216e3aa70ea88bdbe4c5afecb2d87f9
Allon Moritz <allon.moritz@digital-peak.com>
2021-11-25 10:02:02 +0100

    Support J4 plugins without a plugin PHP file

--- webserver/scripts/setup-joomla-site.php
+++ webserver/scripts/setup-joomla-site.php
@@ -12,78 +12,78 @@ $joomlaVersion = array_key_exists(4, $argv) ? $argv[4] : (substr($argv[1], -1) =
 $force         = array_key_exists(5, $argv) ? (bool)$argv[5] : false;
 
 if (is_dir($wwwRoot) && !$force) {
-	return;
+    return;
 }
 
 if (!is_dir($wwwRoot) || $force) {
-	if (!is_dir('/var/www/html/cache') && $hasInternet) {
-		shell_exec('git clone https://github.com/joomla/joomla-cms.git /var/www/html/cache 2>&1 > /dev/null');
-	} else if ($hasInternet) {
-		shell_exec('git --work-tree=/var/www/html/cache --git-dir=/var/www/html/cache/.git fetch origin 2>&1 > /dev/null');
-	} else if (is_dir('/var/www/html/Projects/DPDocker/webserver/www/cache')) {
-		shell_exec('cp -r /var/www/html/Projects/DPDocker/webserver/www/cache /var/www/html/cache');
-	} else {
-		echo 'Can not setup Joomla!!!!' . PHP_EOL;
+    if (!is_dir('/var/www/html/cache') && $hasInternet) {
+        shell_exec('git clone https://github.com/joomla/joomla-cms.git /var/www/html/cache 2>&1 > /dev/null');
+    } elseif ($hasInternet) {
+        shell_exec('git --work-tree=/var/www/html/cache --git-dir=/var/www/html/cache/.git fetch origin 2>&1 > /dev/null');
+    } elseif (is_dir('/var/www/html/Projects/DPDocker/webserver/www/cache')) {
+        shell_exec('cp -r /var/www/html/Projects/DPDocker/webserver/www/cache /var/www/html/cache');
+    } else {
+        echo 'Can not setup Joomla!!!!' . PHP_EOL;
 
-		return;
-	}
+        return;
+    }
 
-	shell_exec('rm -rf ' . $wwwRoot);
-	shell_exec('cp -r /var/www/html/cache ' . $wwwRoot);
+    shell_exec('rm -rf ' . $wwwRoot);
+    shell_exec('cp -r /var/www/html/cache ' . $wwwRoot);
 
-	if ($hasInternet) {
-		$versions = json_decode(file_get_contents('https://downloads.joomla.org/api/v1/latest/cms'));
-		foreach ($versions->branches as $branch) {
-			if ($branch->branch !== 'Joomla! ' . $joomlaVersion) {
-				continue;
-			}
-			// Checkout latest stable release
-			shell_exec('git --work-tree=' . $wwwRoot . ' --git-dir=' . $wwwRoot . '/.git checkout tags/' . $branch->version . ' 2>&1 > /dev/null');
-			echo 'Using version ' . $branch->version . ' on ' . $wwwRoot . PHP_EOL;
-		}
-	}
+    if ($hasInternet) {
+        $versions = json_decode(file_get_contents('https://downloads.joomla.org/api/v1/latest/cms'));
+        foreach ($versions->branches as $branch) {
+            if ($branch->branch !== 'Joomla! ' . $joomlaVersion) {
+                continue;
+            }
+            // Checkout latest stable release
+            shell_exec('git --work-tree=' . $wwwRoot . ' --git-dir=' . $wwwRoot . '/.git checkout tags/' . $branch->version . ' 2>&1 > /dev/null');
+            echo 'Using version ' . $branch->version . ' on ' . $wwwRoot . PHP_EOL;
+        }
+    }
 
-	// Put joomla in dev state so we don't have to delete the installation directory
-	file_put_contents(
-		$wwwRoot . '/libraries/src/Version.php',
-		str_replace("const DEV_STATUS = 'Stable';", "const DEV_STATUS = 'Development';", file_get_contents($wwwRoot . '/libraries/src/Version.php'))
-	);
+    // Put joomla in dev state so we don't have to delete the installation directory
+    file_put_contents(
+        $wwwRoot . '/libraries/src/Version.php',
+        str_replace("const DEV_STATUS = 'Stable';", "const DEV_STATUS = 'Development';", file_get_contents($wwwRoot . '/libraries/src/Version.php'))
+    );
 }
 echo shell_exec('/var/www/html/Projects/DPDocker/webserver/scripts/install-joomla.sh ' . $wwwRoot . ' ' . $db . ' sites_' . $argv[1] . ' "Joomla ' . $argv[1] . '" mailcatcher');
 
 // Check if extensions are needed to be installed
 if (!$argv[2]) {
-	return;
+    return;
 }
 
 $folders = explode(',', $argv[2]);
 if ($argv[2] == 'all') {
-	$folders = array_diff(scandir(dirname(dirname(dirname(__DIR__)))), ['..', '.', 'DPDocker']);
+    $folders = array_diff(scandir(dirname(dirname(dirname(__DIR__)))), ['..', '.', 'DPDocker']);
 }
 
 foreach ($folders as $project) {
-	// Ignore projects with a dash when not a dev site is built and we use all extensions
-	if ($argv[2] == 'all' && strpos($argv[1], 'dev') === false && strpos($project, '-') > 0) {
-		continue;
-	}
+    // Ignore projects with a dash when not a dev site is built and we use all extensions
+    if ($argv[2] == 'all' && strpos($argv[1], 'dev') === false && strpos($project, '-') > 0) {
+        continue;
+    }
 
-	// Ignore all non dev projects when we have a dev site and we use all extensions
-	if ($argv[2] == 'all' && strpos($argv[1], 'dev') !== false && strpos($project, '-Dev') === false) {
-		continue;
-	}
+    // Ignore all non dev projects when we have a dev site and we use all extensions
+    if ($argv[2] == 'all' && strpos($argv[1], 'dev') !== false && strpos($project, '-Dev') === false) {
+        continue;
+    }
 
-	// Check if it is a Joomla installation
-	if (file_exists('/var/www/html/Projects/' . $project . '/includes')) {
-		continue;
-	}
+    // Check if it is a Joomla installation
+    if (file_exists('/var/www/html/Projects/' . $project . '/includes')) {
+        continue;
+    }
 
-	if ($hasInternet && $argv[2] != 'all') {
-		echo 'Building extension ' . $project . PHP_EOL;
-		shell_exec('/var/www/html/Projects/DPDocker/composer/scripts/exec-install.sh ' . $project);
-		shell_exec('/var/www/html/Projects/DPDocker/npm/scripts/exec-npm-install.sh ' . $project . ' 2>/dev/null');
-	}
+    if ($hasInternet && $argv[2] != 'all') {
+        echo 'Building extension ' . $project . PHP_EOL;
+        shell_exec('/var/www/html/Projects/DPDocker/composer/scripts/exec-install.sh ' . $project);
+        shell_exec('/var/www/html/Projects/DPDocker/npm/scripts/exec-npm-install.sh ' . $project . ' 2>/dev/null');
+    }
 
-	createLinks('/var/www/html/Projects/' . $project . '/', $wwwRoot);
+    createLinks('/var/www/html/Projects/' . $project . '/', $wwwRoot);
 }
 echo 'Discovering extensions on ' . $wwwRoot . PHP_EOL;
 copy('/var/www/html/Projects/DPDocker/webserver/scripts/discoverapp.php', $wwwRoot . '/discoverapp.php');
@@ -92,130 +92,134 @@ unlink($wwwRoot . '/discoverapp.php');
 
 function createLinks($folderRoot, $wwwRoot)
 {
-	echo 'Starting to create the links for ' . $folderRoot . PHP_EOL;
+    echo 'Starting to create the links for ' . $folderRoot . PHP_EOL;
 
-	// Folder structure like https://github.com/joomla-extensions/weblinks
-	if (file_exists($folderRoot . '/src/administrator/components/')) {
-		foreach (new DirectoryIterator($folderRoot . '/src/administrator/components/') as $filename) {
-			createLink($folderRoot . '/src/administrator/components/' . $filename, $wwwRoot . '/administrator/components/' . $filename);
-		}
-	}
-	if (file_exists($folderRoot . '/src/administrator/manifests/packages/')) {
-		foreach (new DirectoryIterator($folderRoot . '/src/administrator/manifests/packages/') as $filename) {
-			createLink($folderRoot . '/src/administrator/manifests/packages/' . $filename,
-				$wwwRoot . '/administrator//manifests/packages/' . $filename);
-		}
-	}
-	if (file_exists($folderRoot . '/src/api/components/')) {
-		foreach (new DirectoryIterator($folderRoot . '/src/api/components/') as $filename) {
-			createLink($folderRoot . '/src/api/components/' . $filename, $wwwRoot . '/api/components/' . $filename);
-		}
-	}
-	if (file_exists($folderRoot . '/src/components/')) {
-		foreach (new DirectoryIterator($folderRoot . '/src/components/') as $filename) {
-			createLink($folderRoot . '/src/components/' . $filename, $wwwRoot . '/components/' . $filename);
-		}
-	}
-	if (file_exists($folderRoot . '/src/administrator/language')) {
-		$languages = scandir($folderRoot . '/src/administrator/language');
-		foreach ($languages as $language) {
-			foreach (new DirectoryIterator($folderRoot . '/src/administrator/language/' . $language) as $filename) {
-				createLink($folderRoot . '/src/administrator/language/' . $language . $filename,
-					$wwwRoot . '/administrator/language/' . $language . $filename);
-			}
-		}
-	}
-	if (file_exists($folderRoot . '/src/language')) {
-		$languages = scandir($folderRoot . '/src/language');
-		foreach ($languages as $language) {
-			foreach (new DirectoryIterator($folderRoot . '/src/language/' . $language) as $filename) {
-				createLink($folderRoot . '/src/language/' . $language . $filename, $wwwRoot . '/language/' . $language . $filename);
-			}
-		}
-	}
-	if (file_exists($folderRoot . '/src/media/')) {
-		foreach (new DirectoryIterator($folderRoot . '/src/media/') as $filename) {
-			createLink($folderRoot . '/src/media/' . $filename, $wwwRoot . '/media/' . $filename);
-		}
-	}
-	if (file_exists($folderRoot . '/src/modules/')) {
-		foreach (new DirectoryIterator($folderRoot . '/src/modules/') as $filename) {
-			createLink($folderRoot . '/src/modules/' . $filename, $wwwRoot . '/modules/' . $filename);
-		}
-	}
-	if (file_exists($folderRoot . '/src/plugins')) {
-		$groups = scandir($folderRoot . '/src/plugins');
-		foreach ($groups as $group) {
-			foreach (new DirectoryIterator($folderRoot . '/src/plugins/' . $group) as $filename) {
-				createLink($folderRoot . '/src/plugins/' . $group . '/' . $filename, $wwwRoot . '/plugins/' . $group . '/' . $filename);
-			}
-		}
-	}
-	if (file_exists($folderRoot . '/src/templates/')) {
-		foreach (new DirectoryIterator($folderRoot . '/src/templates/') as $filename) {
-			createLink($folderRoot . '/src/templates/' . $filename, $wwwRoot . '/templates/' . $filename);
-		}
-	}
+    // Folder structure like https://github.com/joomla-extensions/weblinks
+    if (file_exists($folderRoot . '/src/administrator/components/')) {
+        foreach (new DirectoryIterator($folderRoot . '/src/administrator/components/') as $filename) {
+            createLink($folderRoot . '/src/administrator/components/' . $filename, $wwwRoot . '/administrator/components/' . $filename);
+        }
+    }
+    if (file_exists($folderRoot . '/src/administrator/manifests/packages/')) {
+        foreach (new DirectoryIterator($folderRoot . '/src/administrator/manifests/packages/') as $filename) {
+            createLink(
+                $folderRoot . '/src/administrator/manifests/packages/' . $filename,
+                $wwwRoot . '/administrator//manifests/packages/' . $filename
+            );
+        }
+    }
+    if (file_exists($folderRoot . '/src/api/components/')) {
+        foreach (new DirectoryIterator($folderRoot . '/src/api/components/') as $filename) {
+            createLink($folderRoot . '/src/api/components/' . $filename, $wwwRoot . '/api/components/' . $filename);
+        }
+    }
+    if (file_exists($folderRoot . '/src/components/')) {
+        foreach (new DirectoryIterator($folderRoot . '/src/components/') as $filename) {
+            createLink($folderRoot . '/src/components/' . $filename, $wwwRoot . '/components/' . $filename);
+        }
+    }
+    if (file_exists($folderRoot . '/src/administrator/language')) {
+        $languages = scandir($folderRoot . '/src/administrator/language');
+        foreach ($languages as $language) {
+            foreach (new DirectoryIterator($folderRoot . '/src/administrator/language/' . $language) as $filename) {
+                createLink(
+                    $folderRoot . '/src/administrator/language/' . $language . $filename,
+                    $wwwRoot . '/administrator/language/' . $language . $filename
+                );
+            }
+        }
+    }
+    if (file_exists($folderRoot . '/src/language')) {
+        $languages = scandir($folderRoot . '/src/language');
+        foreach ($languages as $language) {
+            foreach (new DirectoryIterator($folderRoot . '/src/language/' . $language) as $filename) {
+                createLink($folderRoot . '/src/language/' . $language . $filename, $wwwRoot . '/language/' . $language . $filename);
+            }
+        }
+    }
+    if (file_exists($folderRoot . '/src/media/')) {
+        foreach (new DirectoryIterator($folderRoot . '/src/media/') as $filename) {
+            createLink($folderRoot . '/src/media/' . $filename, $wwwRoot . '/media/' . $filename);
+        }
+    }
+    if (file_exists($folderRoot . '/src/modules/')) {
+        foreach (new DirectoryIterator($folderRoot . '/src/modules/') as $filename) {
+            createLink($folderRoot . '/src/modules/' . $filename, $wwwRoot . '/modules/' . $filename);
+        }
+    }
+    if (file_exists($folderRoot . '/src/plugins')) {
+        $groups = scandir($folderRoot . '/src/plugins');
+        foreach ($groups as $group) {
+            foreach (new DirectoryIterator($folderRoot . '/src/plugins/' . $group) as $filename) {
+                createLink($folderRoot . '/src/plugins/' . $group . '/' . $filename, $wwwRoot . '/plugins/' . $group . '/' . $filename);
+            }
+        }
+    }
+    if (file_exists($folderRoot . '/src/templates/')) {
+        foreach (new DirectoryIterator($folderRoot . '/src/templates/') as $filename) {
+            createLink($folderRoot . '/src/templates/' . $filename, $wwwRoot . '/templates/' . $filename);
+        }
+    }
 
-	foreach (new DirectoryIterator($folderRoot) as $filename) {
-		if (strpos($filename, 'com_') === 0) {
-			createLink($folderRoot . $filename . '/admin', $wwwRoot . '/administrator/components/' . $filename);
-			createLink($folderRoot . $filename . '/site', $wwwRoot . '/components/' . $filename);
-			createLink($folderRoot . $filename . '/media', $wwwRoot . '/media/' . $filename);
+    foreach (new DirectoryIterator($folderRoot) as $filename) {
+        if (strpos($filename, 'com_') === 0) {
+            createLink($folderRoot . $filename . '/admin', $wwwRoot . '/administrator/components/' . $filename);
+            createLink($folderRoot . $filename . '/site', $wwwRoot . '/components/' . $filename);
+            createLink($folderRoot . $filename . '/media', $wwwRoot . '/media/' . $filename);
 
-			if (file_exists($folderRoot . $filename . '/api')) {
-				createLink($folderRoot . $filename . '/api', $wwwRoot . '/api/components/' . $filename);
-			}
-		}
-		if (strpos($filename, 'mod_') === 0) {
-			createLink($folderRoot . $filename, $wwwRoot . '/modules/' . $filename);
+            if (file_exists($folderRoot . $filename . '/api')) {
+                createLink($folderRoot . $filename . '/api', $wwwRoot . '/api/components/' . $filename);
+            }
+        }
+        if (strpos($filename, 'mod_') === 0) {
+            createLink($folderRoot . $filename, $wwwRoot . '/modules/' . $filename);
 
-			if (file_exists($folderRoot . $filename . '/media')) {
-				createLink($folderRoot . $filename . '/media', $wwwRoot . '/media/' . $filename);
-			}
-		}
-		if (strpos($filename, 'plg_') === 0) {
-			foreach (new RegexIterator(new DirectoryIterator($folderRoot . $filename), "/\\.xml\$/i") as $pluginFile) {
-				$xml = new SimpleXMLElement(file_get_contents($folderRoot . $filename . '/' . $pluginFile));
+            if (file_exists($folderRoot . $filename . '/media')) {
+                createLink($folderRoot . $filename . '/media', $wwwRoot . '/media/' . $filename);
+            }
+        }
+        if (strpos($filename, 'plg_') === 0) {
+            foreach (new RegexIterator(new DirectoryIterator($folderRoot . $filename), "/\\.xml\$/i") as $pluginFile) {
+                $xml = new SimpleXMLElement(file_get_contents($folderRoot . $filename . '/' . $pluginFile));
 
-				foreach ($xml->files->filename as $file) {
-					$plugin = (string)$file->attributes()->plugin;
-					if (!$plugin) {
-						continue;
-					}
+                foreach ($xml->files->children() as $file) {
+                    $plugin = (string)$file->attributes()->plugin;
+                    if (!$plugin) {
+                        continue;
+                    }
 
-					$group = (string)$xml->attributes()->group;
-					if (!is_dir($wwwRoot . '/plugins/' . $group)) {
-						@mkdir($wwwRoot . '/plugins/' . $group, '0777', true);
-						exec('chmod 777 ' . $wwwRoot . '/plugins/' . $group);
-					}
+                    $group = (string)$xml->attributes()->group;
+                    if (!is_dir($wwwRoot . '/plugins/' . $group)) {
+                        @mkdir($wwwRoot . '/plugins/' . $group, '0777', true);
+                        exec('chmod 777 ' . $wwwRoot . '/plugins/' . $group);
+                    }
 
-					createLink($folderRoot . $filename, $wwwRoot . '/plugins/' . $group . '/' . $plugin);
+                    createLink($folderRoot . $filename, $wwwRoot . '/plugins/' . $group . '/' . $plugin);
 
-					if (file_exists($folderRoot . $filename . '/media')) {
-						createLink($folderRoot . $filename . '/media', $wwwRoot . '/media/' . $filename);
-					}
-				}
-			}
-		}
-		if (strpos($filename, 'tmpl_') === 0) {
-			createLink($folderRoot . $filename, $wwwRoot . '/templates/' . str_replace('tmpl_', '', $filename));
-		}
-		if (strpos($filename, 'lib_') === 0) {
-			createLink($folderRoot . $filename, $wwwRoot . '/libraries/' . $filename);
+                    if (file_exists($folderRoot . $filename . '/media')) {
+                        createLink($folderRoot . $filename . '/media', $wwwRoot . '/media/' . $filename);
+                    }
+                }
+            }
+        }
+        if (strpos($filename, 'tmpl_') === 0) {
+            createLink($folderRoot . $filename, $wwwRoot . '/templates/' . str_replace('tmpl_', '', $filename));
+        }
+        if (strpos($filename, 'lib_') === 0) {
+            createLink($folderRoot . $filename, $wwwRoot . '/libraries/' . $filename);
 
-			if (file_exists($folderRoot . $filename . '/media')) {
-				createLink($folderRoot . $filename . '/media', $wwwRoot . '/media/' . $filename);
-			}
-		}
-	}
-	echo 'Finished to create the links for ' . $folderRoot . PHP_EOL;
+            if (file_exists($folderRoot . $filename . '/media')) {
+                createLink($folderRoot . $filename . '/media', $wwwRoot . '/media/' . $filename);
+            }
+        }
+    }
+    echo 'Finished to create the links for ' . $folderRoot . PHP_EOL;
 }
 
 function createLink($source, $target)
 {
-	$source = realpath($source);
+    $source = realpath($source);
 
-	@mkdir(dirname($target), '777', true);
-	shell_exec('ln -sfn ' . $source . ' ' . $target);
+    @mkdir(dirname($target), '777', true);
+    shell_exec('ln -sfn ' . $source . ' ' . $target);
 }

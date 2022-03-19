69b7a49ab21b22e47f420285c233e6a5bf8fcc84
Allon Moritz <allon.moritz@digital-peak.com>
2022-03-19 17:14:12 +0100

    Add PHP 8.1 images (#33)
    * Add PHP 8.1 images
    
    * Remove php 7.3 and add build force option
    
    * also pull

--- images/push.sh
+++ images/push.sh
@@ -4,11 +4,11 @@
 # @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
 
 # Push the images
-docker push digitpeak/dpdocker-cli:7.3
 docker push digitpeak/dpdocker-cli:7.4
 docker push digitpeak/dpdocker-cli:8.0
-docker push digitpeak/dpdocker-cli
-docker push digitpeak/dpdocker-web:7.3
+docker push digitpeak/dpdocker-cli:8.1
+docker push digitpeak/dpdocker-cli:latest
 docker push digitpeak/dpdocker-web:7.4
 docker push digitpeak/dpdocker-web:8.0
-docker push digitpeak/dpdocker-web
+docker push digitpeak/dpdocker-web:8.1
+docker push digitpeak/dpdocker-web:latest
--- tests/run-system-tests.sh
+++ tests/run-system-tests.sh
@@ -20,8 +20,8 @@ b=${b:-chrome}
 
 while [ $# -gt 0 ]; do
 	 if [[ $1 == "-"* ]]; then
-				param="${1/-/}"
-				declare $param="$2"
+		param="${1/-/}"
+		declare $param="$2"
 	 fi
 	shift
 done

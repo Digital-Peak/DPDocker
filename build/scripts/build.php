90b7c493ac48cc38a2826060ad73ac955a618f81
Allon Moritz <allon.moritz@digital-peak.com>
2022-08-29 17:03:20 +0200

    Support single extension builds

--- build/scripts/build.php
+++ build/scripts/build.php
@@ -10,160 +10,191 @@
  */
 class DPDockerReleaseBuild
 {
-    public $extensionRoot;
+	public $extensionRoot;
 
-    public function build()
-    {
-        // Normalize path
-        $this->extensionRoot = realpath($this->extensionRoot);
+	public function build()
+	{
+		// Normalize path
+		$this->extensionRoot = realpath($this->extensionRoot);
 
-        // Cleanup the dist folder
-        $distFolder = dirname($this->extensionRoot) . '/dist';
-        shell_exec('rm -rf ' . $distFolder);
-        mkdir($distFolder);
+		// Cleanup the dist folder
+		$distFolder = dirname($this->extensionRoot) . '/dist';
+		shell_exec('rm -rf ' . $distFolder);
+		mkdir($distFolder);
 
-        // Read the build config
-        $config = json_decode(file_get_contents($this->extensionRoot . '/package/build.json'));
-        foreach ($config->packages as $package) {
-            // Prepare the temp folder
-            $tmpFolder = dirname($this->extensionRoot) . '/build';
-            shell_exec('rm -rf ' . $tmpFolder);
-            mkdir($tmpFolder);
+		// Read the build config
+		$config = json_decode(file_get_contents($this->extensionRoot . '/package/build.json'));
+		foreach ($config->packages as $package) {
+			// Prepare the temp folder
+			$tmpFolder = dirname($this->extensionRoot) . '/build';
+			shell_exec('rm -rf ' . $tmpFolder);
+			mkdir($tmpFolder);
 
-            // Read the manifest file
-            $manifest  = new SimpleXMLElement(file_get_contents($this->extensionRoot . '/package/' . $package->originalManifestFileName . '.xml'));
-            $dpVersion = (string)$manifest->version;
+			// Read the manifest file
+			$manifestFile = $this->extensionRoot . '/package/' . $package->originalManifestFileName . '.xml';
 
-            echo ' Creating version ' . $dpVersion . ' of ' . $package->name . PHP_EOL;
+			// When there is no package file use the normal extension manifest
+			if (!file_exists($manifestFile)){
+				$manifestFile = $this->extensionRoot . '/' . $package->name . '/' . $package->originalManifestFileName . '.xml';
+			}
 
-            $dpVersion = str_replace('.', '_', $dpVersion);
+			$manifest  = new SimpleXMLElement(file_get_contents($manifestFile));
+			$dpVersion = (string)$manifest->version;
 
-            // Collect the extensions which do belong to the package
-            $extensions = [];
+			echo ' Creating version ' . $dpVersion . ' of ' . $package->name . PHP_EOL;
 
-            // Iterate over the files in the manifest
-            foreach ($manifest->files->file as $file) {
-                $extension = null;
-                $id        = (string)$file->attributes()->id;
+			$dpVersion = str_replace('.', '_', $dpVersion);
 
-                if (!empty($package->extensions)) {
-                    // Look for an override in the build config
-                    foreach ($package->extensions as $ex) {
-                        if ($ex->name == $id) {
-                            $extension = $ex;
-                            break;
-                        }
-                    }
-                }
+			// Collect the extensions which do belong to the package
+			$extensions = [];
 
-                // If none override is found, create an extension from the manifest
-                if ($extension == null) {
-                    $extension = (object)['name' => $id];
-                }
-                $extensions[] = $extension;
-            }
+			// Iterate over the files in the manifest
+			foreach ($manifest->files->file as $file) {
+				$extension = null;
+				$id		   = (string)$file->attributes()->id;
 
-            // Loop over the extensions
-            foreach ($extensions as $extension) {
-                // Default the excludes
-                $excludes = [];
-                if (!empty($extension->excludes)) {
-                    foreach ($extension->excludes as $exclude) {
-                        $excludes[] = $extension->name . '/' . $exclude;
-                    }
-                }
+				if (!empty($package->extensions)) {
+					// Look for an override in the build config
+					foreach ($package->extensions as $ex) {
+						if ($ex->name == $id) {
+							$extension = $ex;
+							break;
+						}
+					}
+				}
 
-                // Default the substitutes
-                $substitutes = [];
-                if (!empty($extension->substitutes)) {
-                    foreach ($extension->substitutes as $substitute) {
-                        $substitutes[$extension->name . '/' . $substitute->original] = $extension->name . '/' . $substitute->replace;
-                    }
-                }
+				// If none override is found, create an extension from the manifest
+				if ($extension == null) {
+					$extension = (object)['name' => $id];
+				}
+				$extensions[] = $extension;
+			}
 
-                // Create the extension zip file
-                $this->createZip(
-                    $this->extensionRoot . '/' . $extension->name,
-                    $tmpFolder . '/' . $extension->name . '.zip',
-                    $excludes,
-                    $substitutes
-                );
-            }
+			// When no extensions from package we look in the build config
+			if (!$extensions) {
+				foreach ($package->extensions as $extension) {
+					$extensions[] = $extension;
+				}
+			}
 
-            // Copy some package files to the tmp folder
-            if (is_file($this->extensionRoot . '/package/script.php')) {
-                copy($this->extensionRoot . '/package/script.php', $tmpFolder . '/script.php');
-            }
-            copy($this->extensionRoot . '/License.md', $tmpFolder . '/License.txt');
-            copy(
-                $this->extensionRoot . '/package/' . $package->originalManifestFileName . '.xml',
-                $tmpFolder . '/' . $package->substituteManifestFileName . '.xml'
-            );
+			// Loop over the extensions
+			foreach ($extensions as $extension) {
+				// Default the excludes
+				$excludes = [];
+				if (!empty($extension->excludes)) {
+					foreach ($extension->excludes as $exclude) {
+						$excludes[] = $extension->name . '/' . $exclude;
+					}
+				}
 
-            // Create the package zip file
-            $this->createZip($tmpFolder, $distFolder . '/' . $package->name . '_' . $dpVersion . '.zip', [], []);
-        }
-    }
+				// Default the substitutes
+				$substitutes = [];
+				if (!empty($extension->substitutes)) {
+					foreach ($extension->substitutes as $substitute) {
+						$substitutes[$extension->name . '/' . $substitute->original] = $extension->name . '/' . $substitute->replace;
+					}
+				}
 
-    private function createZip($folder, $zipFile, $excludes, $substitutes)
-    {
-        // Some predefined excludes
-        $excludes[] = 'vendor-ignore.txt';
-        $excludes[] = 'composer.json';
-        $excludes[] = 'composer.lock';
-        $excludes[] = 'css.map';
-        $excludes[] = 'js.map';
+				// Create the extension zip file
+				$this->createZip(
+					$this->extensionRoot . '/' . $extension->name,
+					$tmpFolder . '/' . $extension->name . '.zip',
+					$excludes,
+					$substitutes
+				);
 
-        // The zip objects
-        $zip = new ZipArchive();
-        $zip->open($zipFile, ZIPARCHIVE::CREATE);
+				// When no single archive continue
+				if (count($extensions) > 1) {
+					continue;
+				}
 
-        // The file iterator
-        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder), RecursiveIteratorIterator::LEAVES_ONLY);
-        foreach ($files as $file) {
-            // Get real path for current file
-            $filePath = $file->getRealPath();
-            $fileName = str_replace($this->extensionRoot . '/', '', $filePath);
-            $fileName = str_replace(dirname($this->extensionRoot) . '/build', '', $fileName);
+				// Extract the zip file again as we have it in the main build folder
+				$zip = new ZipArchive();
+				$zip->open($tmpFolder . '/' . $extension->name . '.zip');
+				$zip->extractTo($tmpFolder);
+				$zip->close();
 
-            // Handling top level resources directory special as it can be that vendor has resources too
-            $segments = explode('/', $fileName);
-            if (count($segments) > 1 && $segments[1] == 'resources') {
-                continue;
-            }
+				// Delete the zip file
+				unlink($tmpFolder . '/' . $extension->name . '.zip');
+			}
 
-            // Check if the file should be ignored
-            $ignore = false;
-            foreach ($excludes as $exclude) {
-                if (stripos($fileName, $exclude) !== false) {
-                    $ignore = true;
-                    break;
-                }
-            }
+			// Copy some package files to the tmp folder
+			if (count($extensions) > 1 && is_file($this->extensionRoot . '/package/script.php')) {
+				copy($this->extensionRoot . '/package/script.php', $tmpFolder . '/script.php');
+			}
+			copy($this->extensionRoot . '/License.md', $tmpFolder . '/License.txt');
+			copy($manifestFile, $tmpFolder . '/' . $package->substituteManifestFileName . '.xml');
 
-            // Also ignore directories
-            if ($ignore || is_dir($filePath) || !$fileName) {
-                continue;
-            }
+			// Create the package zip file
+			$this->createZip($tmpFolder, $distFolder . '/' . $package->name . '_' . $dpVersion . '.zip', [], []);
+		}
+	}
 
-            // Doe the substitution
-            if (key_exists($fileName, $substitutes)) {
-                $fileName = $substitutes[$fileName];
-            }
+	private function createZip($folder, $zipFile, $excludes, $substitutes)
+	{
+		// Some predefined excludes
+		$excludes[] = 'vendor-ignore.txt';
+		$excludes[] = 'composer.json';
+		$excludes[] = 'composer.lock';
+		$excludes[] = 'css.map';
+		$excludes[] = 'js.map';
 
-            // Remove trailing slashes
-            $fileName = trim($fileName, '/');
+		// The zip objects
+		$zip = new ZipArchive();
+		$zip->open($zipFile, ZIPARCHIVE::CREATE);
 
-            // Add current file to archive
-            $zip->addFile($filePath, $fileName);
-        }
+		// The file iterator
+		$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder), RecursiveIteratorIterator::LEAVES_ONLY);
+		foreach ($files as $file) {
+			// Get real path for current file
+			$filePath = $file->getRealPath();
+			$fileName = str_replace($this->extensionRoot . '/', '', $filePath);
+			$fileName = str_replace(dirname($this->extensionRoot) . '/build', '', $fileName);
 
-        // Close the zip file
-        $zip->close();
-    }
+			// Handling top level resources directory special as it can be that vendor has resources too
+			$segments = explode('/', $fileName);
+			if (count($segments) > 1 && $segments[1] == 'resources') {
+				continue;
+			}
+
+			// Check if the file should be ignored
+			$ignore = false;
+			foreach ($excludes as $exclude) {
+				if (stripos($fileName, $exclude) !== false) {
+					$ignore = true;
+					break;
+				}
+			}
+
+			// Also ignore directories
+			if ($ignore || is_dir($filePath) || !$fileName) {
+				continue;
+			}
+
+			// Doe the substitution
+			if (key_exists($fileName, $substitutes)) {
+				$fileName = $substitutes[$fileName];
+			}
+
+			// Remove trailing slashes
+			$fileName = trim($fileName, '/');
+
+			// Remove the main extension name so the files are all in the main folder
+			$baseName = basename($folder) . '/';
+			if (strpos($fileName, $baseName) === 0) {
+				$fileName = str_replace($baseName, '', $fileName);
+			}
+
+			// Add current file to archive
+			$zip->addFile($filePath, $fileName);
+		}
+
+		// Close the zip file
+		$zip->close();
+	}
 }
 
 // Instantiate and run the app
-$build                = new DPDockerReleaseBuild();
+$build				  = new DPDockerReleaseBuild();
 $build->extensionRoot = $argv[1];
 $build->build();

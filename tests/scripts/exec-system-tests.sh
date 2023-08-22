c6eb801a56a85e83b81aa03aeef851ef68835186
Allon Moritz <allon.moritz@digital-peak.com>
2023-08-22 15:15:13 +0200

    Pass PHP version to tests

--- tests/scripts/exec-system-tests.sh
+++ tests/scripts/exec-system-tests.sh
@@ -8,7 +8,7 @@ while  ! curl http://web-test  > /dev/null 2>&1; do
 	sleep 4
 done
 
-echo "Running Tests on Joomla $2 with $3"
+echo "Running Tests on Joomla $2 and PHP $4 with $3"
 
 # Setup download dir with correct permissions
 sudo rm -rf /tmp/tests/*
@@ -36,6 +36,7 @@ fi
 
 export CODECEPTION_BROWSER=$3
 export CODECEPTION_JOOMLA_VERSION=$2
+export CODECEPTION_PHP_VERSION=$4
 export CODECEPTION_EXTENSION=$1
 
 # Build the actions class and copy it back
@@ -46,7 +47,7 @@ vendor/bin/codecept build
 mkdir -p $(dirname $0)/../../../$1/tests/src/Support/_generated
 cp -f $(dirname $0)/../tmp/tests/src/Support/_generated/AcceptanceTesterActions.php $(dirname $0)/../../../$1/tests/src/Support/_generated/AcceptanceTesterActions.php
 
-if [[ -d tests/src/Acceptance/Install && -z "$4" ]]; then
+if [[ -d tests/src/Acceptance/Install && -z "$5" ]]; then
 	# Run the install task first
 	vendor/bin/codecept run --env desktop tests/src/Acceptance/Install
 fi
@@ -57,10 +58,10 @@ if [ -d tests/src/Acceptance/Install ]; then
 fi
 
 # Check if there are multiple tests to run
-if [[ ! -z $4 && $4 == *".php:"* ]]; then
-	vendor/bin/codecept run --debug --steps --env desktop $4
+if [[ ! -z $5 && $5 == *".php:"* ]]; then
+	vendor/bin/codecept run --debug --steps --env desktop $5
 	exit 0
 fi
 
 # Run the tests
-vendor/bin/codecept run --env desktop $4
+vendor/bin/codecept run --env desktop $5

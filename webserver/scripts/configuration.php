714cf3d32536af0c4b838c347bf34cf0ff264c03
Allon Moritz <allon.moritz@digital-peak.com>
2023-09-11 09:27:47 +0200

    Use maximum for error reporting

--- webserver/scripts/configuration.php
+++ webserver/scripts/configuration.php
@@ -27,7 +27,7 @@ class JConfig {
 	public $live_site = '';
 	public $secret = 'XgrJSL137VSjPBVn';
 	public $gzip = '0';
-	public $error_reporting = 'development';
+	public $error_reporting = 'maximum';
 	public $helpurl = 'https://help.joomla.org/proxy?keyref=Help{major}{minor}:{keyref}&lang={langcode}';
 	public $ftp_host = '';
 	public $ftp_port = '';

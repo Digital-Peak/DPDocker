429dac8f75153c332ccab151ce57c63ddd02c3e4
Allon Moritz <allon.moritz@digital-peak.com>
2023-10-31 10:44:20 +0100

    Install profiler

--- tests/scripts/exec-system-tests.sh
+++ tests/scripts/exec-system-tests.sh
@@ -25,6 +25,9 @@ cd $(dirname $0)/../tmp
 cp -r $(dirname $0)/../../../$1/tests .
 cp -r $(dirname $0)/../config/extension/* .
 
+# Temporary code when for local development
+cp -rf $(dirname $0)/../../../DPCeption/src/* vendor/digital-peak/dpception/src
+
 sudo sed -i "s/debug = '1'/debug = 0/g" /var/www/html/joomla/configuration.php
 sudo sed -i "s/sef = '1'/sef = 0/g" /var/www/html/joomla/configuration.php
 sudo sed -i "s/smtphost = 'mailcatcher'/smtphost = 'mailcatcher-test'/g" /var/www/html/joomla/configuration.php
@@ -38,10 +41,6 @@ export CODECEPTION_EXTENSION=$1
 vendor/bin/codecept clean
 vendor/bin/codecept build
 
-# Copy generated action tester file back to the extension
-mkdir -p $(dirname $0)/../../../$1/tests/src/Support/_generated
-cp -f $(dirname $0)/../tmp/tests/src/Support/_generated/AcceptanceTesterActions.php $(dirname $0)/../../../$1/tests/src/Support/_generated/AcceptanceTesterActions.php
-
 if [[ -d tests/src/Acceptance/Install && -z "$5" ]]; then
 	# Run the install task first
 	vendor/bin/codecept run --env desktop tests/src/Acceptance/Install
@@ -52,11 +51,18 @@ if [ -d tests/src/Acceptance/Install ]; then
 	rm -rf tests/src/Acceptance/Install
 fi
 
+mkdir $(dirname $0)/../tmp/profile
+mkdir $(dirname $0)/../tmp/profile/mem
+mkdir $(dirname $0)/../tmp/profile/xdebug
+export MEMPROF_PROFILE=1
+export XDEBUG_CONFIG="output_dir=$(realpath $(dirname $0)/../tmp/profile/xdebug)"
+
+args=
+
 # Check if there are multiple tests to run
 if [[ ! -z $5 && $5 == *".php:"* ]]; then
-	vendor/bin/codecept run --debug --steps --env desktop $5
-	exit 0
+	args="--debug --steps"
 fi
+php -dextension=memprof.so vendor/codeception/codeception/app.php run $args --env desktop $5
 
-# Run the tests
-vendor/bin/codecept run --env desktop $5
+cp /tmp/cachegrind* $(dirname $0)/../tmp/profile/mem

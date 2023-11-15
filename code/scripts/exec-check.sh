5912f8070a0796dd20fbcbc8591c7a8840428309
Allon Moritz <allon.moritz@digital-peak.com>
2023-11-15 15:08:34 +0100

    Exit code check with a summary of all status codes

--- code/scripts/exec-check.sh
+++ code/scripts/exec-check.sh
@@ -3,6 +3,8 @@
 # @copyright Copyright (C) 2020 Digital Peak GmbH. <https://www.digital-peak.com>
 # @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
 
+status=0
+
 cd $(dirname $0)/../config
 composer install --quiet
 npm install --silent
@@ -13,7 +15,7 @@ file=$(dirname $0)/../config/.php-cs-fixer.php
 if [ -f $(dirname $0)/../../../$1/.php-cs-fixer.php ]; then
 	file=$(dirname $0)/../../../$1/.php-cs-fixer.php
 fi
-$(dirname $0)/../config/vendor/bin/php-cs-fixer fix --dry-run --path-mode=intersection -v --config $file $(dirname $0)/../../../$1/$2
+$(dirname $0)/../config/vendor/bin/php-cs-fixer fix --dry-run --path-mode=intersection -v --config $file $(dirname $0)/../../../$1/$2 || status=$?
 
 echo -e "\nChecking for Javascript code style issues"
 # Allow the extension to overwrite the default config file
@@ -21,7 +23,7 @@ file=$(dirname $0)/../config/.eslintrc.js
 if [ -f $(dirname $0)/../../../$1/.eslintrc.js ]; then
 	file=$(dirname $0)/../../../$1/.eslintrc.js
 fi
-DEBUG=eslint:cli-engine eslint --no-error-on-unmatched-pattern --config $file --ignore-pattern '**/media/*' --ext .js $(dirname $0)/../../../$1/$2
+DEBUG=eslint:cli-engine eslint --no-error-on-unmatched-pattern --config $file --ignore-pattern '**/media/*' --ext .js $(dirname $0)/../../../$1/$2 || status=$?
 
 echo -e "\nChecking for CSS code style issues"
 # Allow the extension to overwrite the default config file
@@ -29,4 +31,6 @@ file=$(dirname $0)/../config/.stylelintrc.json
 if [ -f $(dirname $0)/../../../$1/.stylelintrc.json ]; then
 	file=$(dirname $0)/../../../$1/.stylelintrc.json
 fi
-npx stylelint --allow-empty-input --formatter verbose "$(dirname $0)/../../../$1/$2/**/*.scss"
+npx stylelint --allow-empty-input --formatter verbose "$(dirname $0)/../../../$1/$2/**/*.scss" || status=$?
+
+exit $status

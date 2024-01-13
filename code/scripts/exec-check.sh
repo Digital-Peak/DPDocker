9cd6b87e7df4da616eceb7e8f5bf7733a1149ac2
Allon Moritz <allon.moritz@digital-peak.com>
2024-01-13 17:53:05 +0100

    No rector on check as it will come properly with PHPStan

--- code/scripts/exec-check.sh
+++ code/scripts/exec-check.sh
@@ -3,29 +3,13 @@
 # @copyright Copyright (C) 2020 Digital Peak GmbH. <https://www.digital-peak.com>
 # @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
 
-export DPDOCKER_EXTENSION_PATH=$(realpath $(dirname $0)/../../../$1)
-
 status=0
 
 cd $(dirname $0)/../config
 composer install --quiet
 npm install --silent
 
-if [ ! -d $(dirname $0)/../tmp ]; then
-	git clone --depth 1 https://github.com/joomla/joomla-cms.git $(dirname $0)/../tmp
-	cd $(dirname $0)/../tmp
-	composer install --no-dev
-	npm install
-fi
-
 echo -e "\nChecking for PHP code style issues"
-# Allow the extension to overwrite the default config file
-file=$(dirname $0)/../config/rector.php
-if [ -f $(dirname $0)/../../../$1/rector.php ]; then
-	file=$(dirname $0)/../../../$1/rector.php
-fi
-$(dirname $0)/../config/vendor/bin/rector process --dry-run --config $file $(dirname $0)/../../../$1/$2 || status=$?
-
 # Allow the extension to overwrite the default config file
 file=$(dirname $0)/../config/.php-cs-fixer.php
 if [ -f $(dirname $0)/../../../$1/.php-cs-fixer.php ]; then

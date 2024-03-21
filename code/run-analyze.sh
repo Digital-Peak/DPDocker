6f44aa631860b10b69f2bf83f4d43d51d29d5df8
Allon Moritz <allon.moritz@digital-peak.com>
2024-03-21 17:05:42 +0100

    Cache PHPStan data

--- code/run-analyze.sh
+++ code/run-analyze.sh
@@ -4,6 +4,7 @@
 # @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
 
 # Ensure cache directory exists
-mkdir -p $(dirname $0)/tmp/cache
+mkdir -p $(dirname $0)/tmp/cache/rector
+mkdir -p $(dirname $0)/tmp/cache/phpstan
 
 EXTENSION=$1 FILE=$2 docker-compose -f $(dirname $0)/docker-compose.yml run --rm analyze

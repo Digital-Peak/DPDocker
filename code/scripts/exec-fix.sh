12f890fd0d26f0d5ec367f92cd71575e8fae0629
Allon Moritz <allon.moritz@digital-peak.com>
2024-05-08 15:20:21 +0200

    update deps

--- code/scripts/exec-fix.sh
+++ code/scripts/exec-fix.sh
@@ -21,7 +21,7 @@ file=$(dirname $0)/../config/.eslintrc.js
 if [ -f $(dirname $0)/../../../$1/.eslintrc.js ]; then
 	file=$(dirname $0)/../../../$1/.eslintrc.js
 fi
-DEBUG=eslint:cli-engine eslint --fix --no-error-on-unmatched-pattern --config $file --ignore-pattern '**/media/*' --ext .js $(dirname $0)/../../../$1/$2
+ESLINT_USE_FLAT_CONFIG=false DEBUG=eslint:cli-engine eslint --fix --no-error-on-unmatched-pattern --config $file --ignore-pattern '**/media/*' --ext .js $(dirname $0)/../../../$1/$2
 
 echo -e "\nFixing CSS code style issues"
 # Allow the extension to overwrite the default config file
@@ -29,6 +29,6 @@ file=$(dirname $0)/../config/.stylelintrc.json
 if [ -f $(dirname $0)/../../../$1/.stylelintrc.json ]; then
 	file=$(dirname $0)/../../../$1/.stylelintrc.json
 fi
-npx stylelint --fix --allow-empty-input --formatter verbose "$(dirname $0)/../../../$1/$2/**/*.scss"
+# npx stylelint --fix --allow-empty-input --formatter verbose "$(dirname $0)/../../../$1/$2/**/*.scss"
 
 find $(dirname $0)/../tmp -type l -delete

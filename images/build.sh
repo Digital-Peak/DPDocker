a773cef85476f368c0507ea7433ad8da505be696
Allon Moritz <allon.moritz@digital-peak.com>
2024-06-26 10:59:39 +0200

    Update npm during images build

--- images/build.sh
+++ images/build.sh
@@ -9,11 +9,11 @@ if [ ! -z $1 ]; then
 fi
 
 # Build the images
-docker build $(dirname $0)/cli $f -t digitpeak/dpdocker-cli:8.2 --build-arg PHP_VERSION=8.1
+docker build $(dirname $0)/cli $f -t digitpeak/dpdocker-cli:8.1 --build-arg PHP_VERSION=8.1
 docker build $(dirname $0)/cli $f -t digitpeak/dpdocker-cli:8.2 --build-arg PHP_VERSION=8.2
 docker build $(dirname $0)/cli $f -t digitpeak/dpdocker-cli:8.3 --build-arg PHP_VERSION=8.3
 docker build $(dirname $0)/cli $f -t digitpeak/dpdocker-cli:latest --build-arg PHP_VERSION=8.3
-docker build $(dirname $0)/web $f -t digitpeak/dpdocker-web:8.2 --build-arg PHP_VERSION=8.1
+docker build $(dirname $0)/web $f -t digitpeak/dpdocker-web:8.1 --build-arg PHP_VERSION=8.1
 docker build $(dirname $0)/web $f -t digitpeak/dpdocker-web:8.2 --build-arg PHP_VERSION=8.2
-docker build $(dirname $0)/web $f -t digitpeak/dpdocker-web:8.2 --build-arg PHP_VERSION=8.3
+docker build $(dirname $0)/web $f -t digitpeak/dpdocker-web:8.3 --build-arg PHP_VERSION=8.3
 docker build $(dirname $0)/web $f -t digitpeak/dpdocker-web:latest --build-arg PHP_VERSION=8.3

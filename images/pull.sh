df0d33dede494458505ccc8220226ec7293c2683
Allon Moritz <allon.moritz@digital-peak.com>
2024-12-06 14:02:12 +0100

    Add PHP 8.4 images

--- images/pull.sh
+++ images/pull.sh
@@ -4,11 +4,11 @@
 # @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
 
 # Pull the images
-docker pull digitpeak/dpdocker-cli:8.1
 docker pull digitpeak/dpdocker-cli:8.2
 docker pull digitpeak/dpdocker-cli:8.3
+docker pull digitpeak/dpdocker-cli:8.4
 docker pull digitpeak/dpdocker-cli:latest
-docker pull digitpeak/dpdocker-web:8.1
 docker pull digitpeak/dpdocker-web:8.2
 docker pull digitpeak/dpdocker-web:8.3
+docker pull digitpeak/dpdocker-web:8.4
 docker pull digitpeak/dpdocker-web:latest
